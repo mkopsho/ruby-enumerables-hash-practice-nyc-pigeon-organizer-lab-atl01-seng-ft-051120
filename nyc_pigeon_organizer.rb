b98115e2a2551663fc6f6075121ecdcc7c3f88ca
Michael Kopsho <michael.kopsho@gmail.com>
2020-04-17 20:04:45 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,16 +1,16 @@
 def nyc_pigeon_organizer(data)
   names_hash = {}
-  data.each_pair do |top_level_key, hash|
-    hash.each_pair do |attribute, array|
-      array.each do |name|
+  data.each_pair do |top_level_key, hash| # We use the each_pair enumerable to give us access to the first nested hash. 
+    hash.each_pair do |attribute, array| # We use each_pair again on the nested hash to give us access to the arrays of names.
+      array.each do |name| # For each `name` element in our array, do the following if statements.
         if !names_hash.has_key?(name)
-          names_hash[name] = {}
+          names_hash[name] = {} # If our new hash doesn't have this `name` element as a key, add it and assign it to an empty hash.
         end
         if !names_hash[name].has_key?(top_level_key)
-          names_hash[name][top_level_key] = []
+          names_hash[name][top_level_key] = [] # If our hash assigned to `name` doesn't have this top level key in its hash, add it and assign it an empty array.
         end
         if !names_hash[name][top_level_key].include?(attribute)
-          names_hash[name][top_level_key] << attribute.to_s
+          names_hash[name][top_level_key] << attribute.to_s # If our array assigned to a top level key doesn't have this attribute in it, add it and convert it into a string.
         end
       end
     end

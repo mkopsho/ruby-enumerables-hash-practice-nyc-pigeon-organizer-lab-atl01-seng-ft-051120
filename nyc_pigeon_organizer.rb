2d53300823685355a4aed4d40f5c62748aeaaaf9
Michael Kopsho <michael.kopsho@gmail.com>
2020-04-17 19:23:44 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,9 +1,14 @@
 def nyc_pigeon_organizer(data)
   names_array = []
+  pigeon_hash = {}
   data.each_pair do |key, value|
     value.each do |k, v|
       names_array << v
     end
   end
-  names_array = names_array.flatten && names_array.uniq
-ende
+  names_array = names_array.flatten
+  names_array = names_array.uniq
+  
+  names_array.collect do |name|
+    
+end

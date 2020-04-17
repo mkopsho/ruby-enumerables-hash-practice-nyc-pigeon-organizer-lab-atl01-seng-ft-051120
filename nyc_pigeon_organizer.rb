88c26fb66d3d6cc0da9fb6789516111397b83b5e
Michael Kopsho <michael.kopsho@gmail.com>
2020-04-17 19:43:51 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,8 +1,9 @@
 def nyc_pigeon_organizer(data)
   names_hash = {}
-  data.each_pair do |top_level_key, value|
+  data.each_pair do |top_level_keys, value|
     value.each do |key, array|
       array.each do |name|
         if names_hash == nil
           hash{name}
+          hash[name][top_level_keys]
 end

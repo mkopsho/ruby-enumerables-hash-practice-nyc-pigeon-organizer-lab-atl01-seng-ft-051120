58fc17bee40af0bc5d327cafe913958eefe30c2e
Michael Kopsho <michael.kopsho@gmail.com>
2020-04-17 19:53:27 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,4 +6,11 @@ def nyc_pigeon_organizer(data)
         if !names_hash.has_key?(name)
           names_hash[name] = {}
         end
+        if !new_hash[name].has_key?()
+          new_hash[name][property] = []
+        end
+
+        if !new_hash[name][property].include?(attribute)
+          new_hash[name][property] << attribute.to_s
+        end
 end
